a7524333a1d21ccf3f8d05a68868b3a9adcc3d4e
Timothy Miller <innerlogic4321@gmail.com>
2025-11-18 15:44:14 +0900

    Sandbox App

---
+++ samples/SandboxApp/MauiAppStub.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls.Maui;
+using Avalonia.Controls.Maui.Platform;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace SandboxApp;
+
+public partial class MauiAppStub : Application
+{
+	public MauiAppStub()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+    }
+}
---
+++ samples/SandboxApp/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls.Maui;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Platform;
+
+namespace SandboxApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp
+            .CreateBuilder();
+
+        builder
+             .UseMauiApp<MauiAppStub>()
+             .UseAvaloniaApp()
+             .UseAvaloniaGraphics()
+             .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("bpg-banner-webfont.ttf", "BgpBanner");
+            });
+
+
+        return builder.Build();
+    }
+}
+
