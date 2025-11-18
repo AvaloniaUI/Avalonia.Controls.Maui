e66ad313bcaa19f8bff096d5a2ff8800c9c1182d
Timothy Miller <innerlogic4321@gmail.com>
2025-11-18 22:01:21 +0900

    WASM

---
+++ samples/ControlGallery/ControlGallery.Browser/Program.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Browser;
+using Avalonia.Controls;
+using Avalonia.Controls.Maui;
+using System;
+using System.Threading.Tasks;
+
+namespace ControlGallery;
+
+internal sealed partial class Program
+{
+    private static Task Main(string[] args) => BuildAvaloniaApp()
+            .WithInterFont()
+            .WithMauiFonts()
+            .StartBrowserAppAsync("out");
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<AvaloniaApp>();
+}
