553a4e1eac118bc12ca08df7e7c5ffc573628bd8
Timothy Miller <innerlogic4321@gmail.com>
2025-11-22 13:12:24 +0900

    Fonts page

---
+++ samples/ControlGallery/ControlGallery/Pages/FontsPage.xaml.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.Maui.Controls;
+
+namespace ControlGallery.Pages;
+
+public partial class FontsPage : ContentPage
+{
+    public FontsPage()
+    {
+        InitializeComponent();
+    }
+}
