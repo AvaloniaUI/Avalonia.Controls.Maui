e0d05a21737eaf8109056ce2aa9aa44b5e3d0b70
Timothy Miller <innerlogic4321@gmail.com>
2025-11-24 12:09:10 +0900

    WeatherTwentyOne

---
+++ samples/WeatherTwentyOne/MauiAppStub.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Avalonia.Controls.Maui;
+using Avalonia.Controls.Maui.Platform;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace WeatherTwentyOne;
+
+public partial class MauiAppStub : Application
+{
+    public MauiAppStub()
+    {
+        InitializeComponent();
+    }
+
+    // protected override Window CreateWindow(IActivationState? activationState)
+    // {
+    //     return new Window(new AppShell());
+    // }
+
+    async void TapGestureRecognizer_Tapped(System.Object sender, System.EventArgs e)
+    {
+        try
+        {
+            await Shell.Current.GoToAsync($"///settings");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"err: {ex.Message}");
+        }
+    }
+}
---
+++ samples/WeatherTwentyOne/MauiProgram.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls.Maui;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Platform;
+using WeatherTwentyOne.Pages;
+using WeatherTwentyOne.ViewModels;
+
+namespace WeatherTwentyOne;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp
+            .CreateBuilder();
+
+        builder
+             .UseMauiApp<MauiAppStub>()
+             .UseAvaloniaApp()
+             .UseAvaloniaGraphics()
+#if WINDOWS
+        //lifecycle
+        //    .AddWindows(windows =>
+        //        windows.OnNativeMessage((app, args) => {
+        //            if (WindowExtensions.Hwnd == IntPtr.Zero)
+        //            {
+        //                WindowExtensions.Hwnd = args.Hwnd;
+        //                WindowExtensions.SetIcon("Platforms/Windows/trayicon.ico");
+        //            }
+        //        }));
+
+            lifecycle.AddWindows(windows => windows.OnWindowCreated((del) => {
+                del.ExtendsContentIntoTitleBar = true;
+            }));
+#endif
+             .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("fa-solid-900.ttf", "FontAwesome");
+            });
+
+        var services = builder.Services;
+
+        services.AddSingleton<HomeViewModel>();
+        services.AddSingleton<HomePage>();
+
+        return builder.Build();
+    }
+}
+
