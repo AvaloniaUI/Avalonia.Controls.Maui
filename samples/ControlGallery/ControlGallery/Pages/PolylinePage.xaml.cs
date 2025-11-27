388739f034bb48b34646c5718709b5418cfee133
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-11-27 09:00:17 +0100

    More samples

--- samples/ControlGallery/ControlGallery/Pages/PolylinePage.xaml.cs
+++ samples/ControlGallery/ControlGallery/Pages/PolylinePage.xaml.cs
@@ -1,9 +1,49 @@
+using Microsoft.Maui.Controls;
+
 namespace ControlGallery.Pages;
 
 public partial class PolylinePage : ContentPage
 {
+    private Animation? _dashAnimation;
+
     public PolylinePage()
     {
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// Starts the dash offset animation when the page appears.
+    /// </summary>
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (AnimatedPolyline != null && _dashAnimation == null)
+        {
+            _dashAnimation = new Animation(v =>
+            {
+                // Ensure dash updates happen on the UI thread to avoid cross-thread exceptions.
+                if (Dispatcher.IsDispatchRequired)
+                {
+                    Dispatcher.Dispatch(() => AnimatedPolyline.StrokeDashOffset = v);
+                }
+                else
+                {
+                    AnimatedPolyline.StrokeDashOffset = v;
+                }
+            }, 0, 16, Easing.Linear);
+            _dashAnimation.Commit(this, "PolylineDashOffset", length: 1600, repeat: () => true);
+        }
+    }
+
+    /// <summary>
+    /// Stops the dash offset animation when the page is disappearing.
+    /// </summary>
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        this.AbortAnimation("PolylineDashOffset");
+        _dashAnimation = null;
     }
 }
