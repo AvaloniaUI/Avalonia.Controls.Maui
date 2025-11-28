94a10f212026cc2810212705129ca311bc3f7ded
Timothy Miller <innerlogic4321@gmail.com>
2025-11-28 12:04:09 +0900

    Update

--- src/Avalonia.Controls.Maui/Handlers/BoxViewHandler.cs
+++ src/Avalonia.Controls.Maui/Handlers/BoxViewHandler.cs
@@ -9,6 +9,7 @@ public class BoxViewHandler : ViewHandler<BoxView, Border>
     public static IPropertyMapper<BoxView, BoxViewHandler> Mapper = new PropertyMapper<BoxView, BoxViewHandler>(ViewHandler.ViewMapper)
     {
         [nameof(BoxView.Color)] = MapColor,
+        [nameof(IView.Background)] = MapColor,
         [nameof(BoxView.CornerRadius)] = MapCornerRadius,
     };
     
