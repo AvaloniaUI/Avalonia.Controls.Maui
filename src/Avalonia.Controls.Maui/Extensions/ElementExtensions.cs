37ee0e07b12c0f92891998039b2d73ed05afed5a
Timothy Miller <innerlogic4321@gmail.com>
2025-11-29 09:16:25 +0900

    Set default padding

--- src/Avalonia.Controls.Maui/Extensions/ElementExtensions.cs
+++ src/Avalonia.Controls.Maui/Extensions/ElementExtensions.cs
@@ -4,6 +4,10 @@ public static class ElementExtensions
 {
     public static global::Avalonia.Thickness ToThickness(this Microsoft.Maui.Thickness thickness)
     {
-        return new global::Avalonia.Thickness(thickness.Left, thickness.Top, thickness.Right, thickness.Bottom);
+        return new global::Avalonia.Thickness(
+                        double.IsNaN(thickness.Left) ? 0 : thickness.Left,
+                        double.IsNaN(thickness.Top) ? 0 : thickness.Top,
+                        double.IsNaN(thickness.Right) ? 0 : thickness.Right,
+                        double.IsNaN(thickness.Bottom) ? 0 : thickness.Bottom);
     }
 }
