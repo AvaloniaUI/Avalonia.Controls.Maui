2114eafc07bbcc63da4596dcbd45441632c9bc56
Tim Miller <innerlogic4321@gmail.com>
2025-12-10 20:43:32 +0900

    Remove WithControls extension

--- samples/ControlGallery/ControlGallery.Browser/Program.cs
+++ samples/ControlGallery/ControlGallery.Browser/Program.cs
@@ -11,7 +11,6 @@ internal sealed partial class Program
 {
     private static Task Main(string[] args) => BuildAvaloniaApp()
             .WithInterFont()
-            .WithControls()
             .StartBrowserAppAsync("out");
 
     public static AppBuilder BuildAvaloniaApp()
