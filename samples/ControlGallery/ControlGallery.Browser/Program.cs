21905f72305a525d4009232e799ef1dfb00ae9cd
Timothy Miller <innerlogic4321@gmail.com>
2025-12-10 21:19:39 +0900

    Fix fonts

--- samples/ControlGallery/ControlGallery.Browser/Program.cs
+++ samples/ControlGallery/ControlGallery.Browser/Program.cs
@@ -2,6 +2,7 @@
 using Avalonia.Browser;
 using Avalonia.Controls;
 using Avalonia.Controls.Maui;
+using Avalonia.Media;
 using System;
 using System.Threading.Tasks;
 
@@ -9,10 +10,30 @@ namespace ControlGallery;
 
 internal sealed partial class Program
 {
-    private static Task Main(string[] args) => BuildAvaloniaApp()
-            .WithInterFont()
-            .StartBrowserAppAsync("out");
+        private static Task Main(string[] args) => BuildAvaloniaApp()
+                .WithInterFont()
+                .With(
+                  new FontManagerOptions
+                  {
+                        DefaultFamilyName = "avares://ControlGallery.Browser/Assets#Noto Sans",
+                        FontFallbacks = new[]
+                        {
+                                new FontFallback
+                                {
+                                        FontFamily = new FontFamily("avares://ControlGallery.Browser/Assets#Noto Sans"),
+                                },
+                                 new FontFallback
+                                {
+                                        FontFamily = new FontFamily("avares://ControlGallery.Browser/Assets#Noto Sans JP"),
+                                },
+                                new FontFallback
+                                {
+                                        FontFamily = new FontFamily("avares://ControlGallery.Browser/Assets#Noto Mono"),
+                                },
+                        },
+                  })
+                .StartBrowserAppAsync("out");
 
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<AvaloniaApp>();
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<AvaloniaApp>();
 }
