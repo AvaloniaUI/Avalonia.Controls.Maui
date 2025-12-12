89a89ae17d4d23bd520005de9f9f1eb671e4ce5a
Timothy Miller <innerlogic4321@gmail.com>
2025-12-12 16:57:56 +0900

    Add Native heads for AlohaKit and ControlGallery

---
+++ showcase/AlohaKit.Gallery/AlohaKit.Gallery.Mac/AppDelegate.cs
@@ -0,0 +1,9 @@
+using Foundation;
+
+namespace AlohaKit.Gallery.Mac;
+
+[Register(nameof(AppDelegate))]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
