fc4d9ce13e6071f1ff1ad6f0c926d730076ec104
Timothy Miller <innerlogic4321@gmail.com>
2026-01-14 15:59:35 +0900

    Fix AOT

--- showcase/AlohaKit.Gallery/AlohaKit.Gallery/Views/Base/BasePage.cs
+++ showcase/AlohaKit.Gallery/AlohaKit.Gallery/Views/Base/BasePage.cs
@@ -1,11 +1,38 @@
 ﻿using AlohaKit.Gallery.Models;
 using System.Windows.Input;
 
+// View types for AOT page factory
+using AlohaKit.Gallery;
+
 namespace AlohaKit.Gallery.Views.Base
 {
 	public class BasePage : ContentPage
 	{
 		SectionModel? _selectedItem;
+
+		// Static page factory for AOT compatibility - no reflection
+		private static readonly Dictionary<Type, Func<Page>> PageFactory = new()
+		{
+			[typeof(AvatarView)] = () => new AvatarView(),
+			[typeof(LoadingView)] = () => new LoadingView(),
+			[typeof(ButtonView)] = () => new ButtonView(),
+			[typeof(CaptchaView)] = () => new CaptchaView(),
+			[typeof(CheckBoxView)] = () => new CheckBoxView(),
+			[typeof(LinearGaugeView)] = () => new LinearGaugeView(),
+			[typeof(NumericUpDownView)] = () => new NumericUpDownView(),
+			[typeof(PieChartView)] = () => new PieChartView(),
+			[typeof(PulseIconView)] = () => new PulseIconView(),
+			[typeof(ProgressBarView)] = () => new ProgressBarView(),
+			[typeof(ProgressRadialView)] = () => new ProgressRadialView(),
+			[typeof(SegmentedControlView)] = () => new SegmentedControlView(),
+			[typeof(RatingView)] = () => new RatingView(),
+			[typeof(SliderView)] = () => new SliderView(),
+			[typeof(ToggleSwitchView)] = () => new ToggleSwitchView(),
+			[typeof(BarChartView)] = () => new BarChartView(),
+			[typeof(LineChartView)] = () => new LineChartView(),
+			[typeof(MultiBarChartView)] = () => new MultiBarChartView(),
+			[typeof(MultiLineChartView)] = () => new MultiLineChartView(),
+		};
 
 		public BasePage()
 		{
@@ -34,7 +61,8 @@ namespace AlohaKit.Gallery.Views.Base
 
 		Page PreparePage(SectionModel model)
 		{
-			var page = (Handler?.MauiContext?.Services?.GetService(model.Type) as Page) ?? (Page)Activator.CreateInstance(model.Type)!;
+			var page = (Handler?.MauiContext?.Services?.GetService(model.Type) as Page)
+				?? (PageFactory.TryGetValue(model.Type, out var factory) ? factory() : throw new InvalidOperationException($"No factory registered for {model.Type}"));
 			page.Title = model.Title;
 
 			return page;
