d83d12181634dc08747441c03184661270e419b4
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2026-01-23 13:03:42 +0100

    More fixes

--- tests/Avalonia.Controls.Maui.RenderTests/App.cs
+++ tests/Avalonia.Controls.Maui.RenderTests/App.cs
@@ -8,8 +8,7 @@ public class App : Application
     public override void Initialize()
     {
         RequestedThemeVariant = ThemeVariant.Light;
-        Styles.Add(new FluentTheme());
-        Styles.Add(new Avalonia.Themes.Simple.SimpleTheme());
+        Styles.Add(new Themes.Simple.SimpleTheme());
         Styles.Add(new Avalonia.Markup.Xaml.Styling.StyleInclude(new Uri("avares://Avalonia.Controls.Maui/Styles/Controls.axaml"))
         {
             Source = new Uri("avares://Avalonia.Controls.Maui/Styles/Controls.axaml")
