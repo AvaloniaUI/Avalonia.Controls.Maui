6cc5882f71778feee9620dba60a51bde9d149768
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2026-01-29 10:45:29 +0100

    Add Search Handler item template sample

--- samples/ControlGallery/ControlGallery/Pages/Shell/Playground/ShellSearchPage.xaml.cs
+++ samples/ControlGallery/ControlGallery/Pages/Shell/Playground/ShellSearchPage.xaml.cs
@@ -26,5 +26,56 @@ namespace ControlGallery.Pages.ShellSamples.ShellPlayground
                 handler.Query = string.Empty;
             }
         }
+
+        private void OnSetCustomItemTemplate(object sender, EventArgs e)
+        {
+            var handler = Shell.GetSearchHandler(this);
+            if (handler != null)
+            {
+                handler.ItemTemplate = new DataTemplate(() =>
+                {
+                    var grid = new Grid
+                    {
+                        ColumnDefinitions = new ColumnDefinitionCollection
+                        {
+                            new ColumnDefinition { Width = GridLength.Auto },
+                            new ColumnDefinition { Width = GridLength.Star }
+                        },
+                        Padding = new Thickness(10),
+                        ColumnSpacing = 10
+                    };
+
+                    var icon = new Image
+                    {
+                        Source = "dotnet_bot.png",
+                        WidthRequest = 32,
+                        HeightRequest = 32,
+                        VerticalOptions = LayoutOptions.Center
+                    };
+
+                    var label = new Label
+                    {
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = 16,
+                        VerticalOptions = LayoutOptions.Center
+                    };
+                    label.SetBinding(Label.TextProperty, "Name");
+
+                    grid.Add(icon, 0, 0);
+                    grid.Add(label, 1, 0);
+
+                    return grid;
+                });
+            }
+        }
+
+        private void OnResetItemTemplate(object sender, EventArgs e)
+        {
+            var handler = Shell.GetSearchHandler(this);
+            if (handler != null)
+            {
+                handler.ItemTemplate = null;
+            }
+        }
     }
 }
