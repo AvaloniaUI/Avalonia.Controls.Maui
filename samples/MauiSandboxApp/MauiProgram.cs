a133a0472674afb774e8c615d2c104ce7f966787
Timothy Miller <innerlogic4321@gmail.com>
2026-02-26 10:31:07 +0900

    Use for default TFM

--- samples/MauiSandboxApp/MauiProgram.cs
+++ samples/MauiSandboxApp/MauiProgram.cs
@@ -11,7 +11,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			#if NET
+			#if !IOS && !MACCATALYST && !ANDROID && !WINDOWS
 			 .UseAvaloniaApp()
              .UseAvaloniaGraphics()
 			 .UseAvaloniaMapsui()
