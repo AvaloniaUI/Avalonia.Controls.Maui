4eaecdf444d4c559470cddf762cf453fa5989d84
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2026-03-24 12:35:46 +0100

    Remove console logs

--- src/Avalonia.Controls.Maui.Essentials/Devices/AvaloniaDeviceDisplay.Browser.js
+++ src/Avalonia.Controls.Maui.Essentials/Devices/AvaloniaDeviceDisplay.Browser.js
@@ -1,28 +1,15 @@
 export function requestWakeLock() {
     if ('wakeLock' in navigator) {
-        console.log("Requesting Screen Wake Lock...");
         return navigator.wakeLock.request('screen')
-            .then(sentinel => {
-                console.log("Screen Wake Lock acquired.");
-                return sentinel;
-            })
-            .catch(err => {
-                console.error(`Failed to acquire Screen Wake Lock: ${err.message}`);
-                return null;
-            });
-    } else {
-        console.warn("Screen Wake Lock API is not supported in this browser.");
+            .then(sentinel => sentinel)
+            .catch(() => null);
     }
     return Promise.resolve(null);
 }
 
 export function releaseWakeLock(sentinel) {
     if (sentinel) {
-        console.log("Releasing Screen Wake Lock...");
-        return sentinel.release()
-            .then(() => {
-                console.log("Screen Wake Lock released.");
-            });
+        return sentinel.release();
     }
     return Promise.resolve();
 }
